9f95347c9104f0e44232f1b05bc91005a7a9a52c
madsermad <mban16@student.aau.dk>
2019-05-17 09:11:48 +0200

    Determine the order, estimation and residual analysis of an AR(p) model.

---
+++ AR(p) determination.R
@@ -0,0 +1,30 @@
+### Packages 
+library(tseries)
+library(readxl)
+library(astsa)
+library(fracdiff)
+
+### Beforehand definitions
+Eldaily2014_18 <- read_excel("Eldaily2014-2018.xlsx", 
+                             col_types = c("numeric", "blank", "blank", 
+                                           "blank", "blank", "blank", "blank", 
+                                           "numeric", "numeric", "blank", "blank", 
+                                           "blank", "blank", "blank", "blank", 
+                                           "blank", "blank", "blank", "blank"))
+colnames(Eldaily2014_18)[1] <- "Date"
+dk1f <- Eldaily2014_18[1:2] 
+dk1f[2] <- dk1f[2]-mean(unlist(dk1f[2])) # We are working with this
+
+### Figure 4.3 
+aic <- c() 
+for (i in 0:12) { # Calculating the AIC for each lag
+  aic[i] <- arima(ts(dk1f[2],frequency = 7),c(i,0,0),include.mean = FALSE)$aic
+}
+bic <- c()
+for (i in 0:12) { # Calculating the BIC for each lag
+  bic[i] <- BIC(arima(dk1f[2],c(i,0,0),include.mean = FALSE))
+}
+plot(aic, type = "l", ylab = "AIC and BIC values", xlab = "Order of p", ylim = c(19400,19650)) # The AIC
+lines(bic, col = "red") # The BIC
+points(7,min(bic)); points(7,aic[7]); points(12,aic[12]) # Making minimum points
+text(locator(), labels = c("AIC", "BIC")) # Press the graph to add AIC and BIC, then press esc.
